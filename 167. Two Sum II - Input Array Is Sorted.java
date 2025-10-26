cc622b7619f0fd28e7054e1016a30e2c505b8fd9
Shubham Solanki <shubhamsolanki0x@gmail.com>
2025-10-26 19:46:20 +0530

    Update 167. Two Sum II - Input Array Is Sorted.java

--- 167. Two Sum II - Input Array Is Sorted.java
+++ 167. Two Sum II - Input Array Is Sorted.java
@@ -1,20 +1,15 @@
 ##Brute Force Approach TC -O(N2) Sc - O(1)
-class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int left = 0, right = numbers.length - 1;
-
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-
-            if (sum == target) {
-                return new int[]{left + 1, right + 1}; // 1-based indices
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j }; 
+                }
             }
         }
-        return new int[]{-1, -1}; // No solution found
+        
+        return new int[] {}; 
     }
 }
 
